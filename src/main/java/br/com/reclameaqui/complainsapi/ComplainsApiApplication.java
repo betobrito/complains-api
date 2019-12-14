443595c9af168f66b93c17142be5cbd4dd856ffb
betobrito <Beto0529!>
2019-12-14 15:02:19 -0300

    Initial project structure.

---
+++ src/main/java/br/com/reclameaqui/complainsapi/ComplainsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.reclameaqui.complainsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplainsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplainsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/reclameaqui/complainsapi/ComplainsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.reclameaqui.complainsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplainsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
