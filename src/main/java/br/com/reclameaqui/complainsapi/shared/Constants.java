63a50525fe2e866ceac5a626f9d27b9f5a3e70ff
betobrito <Beto0529!>
2019-12-17 21:29:51 -0300

    - allowed access to consume endpoint without need for jwt token - refatoring

--- src/main/java/br/com/reclameaqui/complainsapi/shared/Constants.java
+++ src/main/java/br/com/reclameaqui/complainsapi/shared/Constants.java
@@ -3,7 +3,7 @@ package br.com.reclameaqui.complainsapi.shared;
 public interface Constants {
 
     String BAR = "/";
-    String API_COMPLAINT = "/complaint";
+    String API_COMPLAINT = "/api/complaint";
 
     interface Messages {
         String MSG_NO_COMPLAINTS_FOUND = "No complaints found with this id.";
